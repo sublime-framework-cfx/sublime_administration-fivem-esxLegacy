b7a2dc97d8fa243450b7700db7404dbbf69cf4f9
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-11-28 18:12:26 +0100

    tweak(fxmanifest): remove experimental_fx_oal

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-use_experimental_fxv2_oal 'yes'
+-- use_experimental_fxv2_oal 'yes'
 version '1.7'
-
 
 client_scripts {
     'modules/rageui/RMenu.lua',
